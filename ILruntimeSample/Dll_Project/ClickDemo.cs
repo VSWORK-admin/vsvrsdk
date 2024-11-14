a150466512bdced2b5ceaf2f381f5331abacc4df
nizhuwei <18861120231@163.com>
2024-11-14 20:08:54 +0800

    缩小或放大点击物体

--- ILruntimeSample/Dll_Project/ClickDemo.cs
+++ ILruntimeSample/Dll_Project/ClickDemo.cs
@@ -16,12 +16,14 @@ namespace Dll_Project
 
         public GameObject ClickedObj;
 
+        private Tweener tween;
 
         public string clickedName = string.Empty;
 
         public string Recieve_A = string.Empty;
         public string Recieve_B = string.Empty;
 
+        private bool isZoomIn = false;
         public override void Init()
         {
             Debug.Log("Click_Demo Init !");
@@ -66,18 +68,51 @@ namespace Dll_Project
             string appname = VSEngine.Instance.GetAppName();
             Debug.Log("VSEngine version " + version + " appname " + appname);
 
+            isZoomIn = !isZoomIn;
+            RoomSycnData roomSycnData = new RoomSycnData()
+            {
+                a = "GameObjectScaleChange",
+                b = ClickedObj.name,
+                c = isZoomIn.ToString()
+            };
+            VSEngine.Instance.SendRoomSyncData(roomSycnData);
         }
 
         void RecieveCChangeObj(RoomSycnData rinfo)
         {
-            Recieve_A = rinfo.a;
-            Recieve_B = rinfo.b;
+           
 
-            HandleCChangeObj();
+            HandleCChangeObj(rinfo);
         }
-        void HandleCChangeObj()
+        void HandleCChangeObj(RoomSycnData rinfo)
         {
+            switch (rinfo.a)
+            {
+                case "GameObjectScaleChange":
+                    GameObject gameObject = GameObject.Find(rinfo.b);
+                    if (gameObject!=null)
+                    {
+                        if (bool.TryParse(rinfo.c,out isZoomIn))
+                        {
+                            if (isZoomIn)
+                            {
+                                tween = gameObject.GetComponent<Transform>().DOScale(Vector3.one * 2, 0.5f);
 
+                                tween.SetAutoKill(true);
+                            }
+                            else
+                            {
+                                tween = gameObject.GetComponent<Transform>().DOScale(Vector3.one , 0.5f);
+
+                                tween.SetAutoKill(true);
+                            }
+                        }
+                       
+                    }
+                    break;
+                default:
+                    break;
+            }
         }
  
     }
