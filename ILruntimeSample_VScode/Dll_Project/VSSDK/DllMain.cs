9f7e5c8f2b70b321b10c1cdc39ad04064d2d2bd0
nizhuwei <nizhuwei@vswork.com>
2025-04-25 19:23:45 +0800

    VSCODE_DllMain修改上传

--- ILruntimeSample_VScode/Dll_Project/VSSDK/DllMain.cs
+++ ILruntimeSample_VScode/Dll_Project/VSSDK/DllMain.cs
@@ -9,7 +9,14 @@ namespace Dll_Project
         public static void Main()
         {
             UnityEngine.Debug.Log("Dll Run Main !");
-
+            GeneralDllBehaviorAdapter[] generalDllBehaviorAdapters = DllManager.Instance.transform.GetComponentsInChildren<GeneralDllBehaviorAdapter>(true);
+            if (generalDllBehaviorAdapters != null)
+            {
+                for (int i = 0; i < generalDllBehaviorAdapters.Length; i++)
+                {
+                    generalDllBehaviorAdapters[i].DllName = "";
+                }
+            }
             foreach (var obj in DllManager.Instance.ExtralDatas)
             {
                 if (obj.Target != null)
